78bf361915a36bdec5ddbf1d905718fde00ae01a
HsH <roeehds2@gmail.com>
2026-04-23 11:59:06 +0900

    광고 ID 재설정

--- components/AdBanner.jsx
+++ components/AdBanner.jsx
@@ -29,7 +29,7 @@ export default function AdBanner({
             <ins
                 className="adsbygoogle"
                 style={style}
-                data-ad-client="ca-pub-YOUR_PUBLISHER_ID"
+                data-ad-client="ca-pub-6320749220035323"
                 data-ad-slot={adSlot}
                 data-ad-format={format}
                 data-full-width-responsive={responsive}
