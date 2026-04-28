eb9317a9326a8aa3c37a72e30c48297094d654fe
hsh <roeehds2@gmail.com>
2026-04-28 20:34:48 +0900

    아이템 수조정(3)

--- components/ItemBar.jsx
+++ components/ItemBar.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+const joker = pool.filter((item) => item.type === "joker");
+const rareOnly = pool.filter(
+    (item) => item.rarity === "rare" && item.type !== "joker"
+);
+const normalOnly = pool.filter(
+    (item) => item.rarity !== "rare" && item.type !== "joker"
+);
 
+const selected = [
+    ...pick(joker, 1),
+    ...pick(rareOnly, 1),
+    ...pick(normalOnly, 3),
+].map((item, index) => ({
+    ...item,
+    instanceId: `${item.id}-${Date.now()}-${index}`,
+    used: false,
+}));
+
+console.log("선택된 아이템:", selected.map((item) => item.id));
+
+setInventory(selected);
 export default function ItemBar({
     inventory = [],
     onUseItem,
@@ -20,7 +40,11 @@ export default function ItemBar({
                     >
                         <div className="item-slot__name">{item.name}</div>
                         <div className="item-slot__meta">
-                            {item.type === "joker" ? "조커" : item.rarity === "rare" ? "희귀" : "일반"}
+                            {item.type === "joker"
+                                ? "조커"
+                                : item.rarity === "rare"
+                                    ? "희귀"
+                                    : "일반"}
                         </div>
                     </button>
                 );
